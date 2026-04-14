4735805c65a5e6f4ca60414c0581559823858b30
yuhang0824 <hangcy0824@gmail.com>
2026-04-15 02:35:58 +0800

    update home page

--- Production/home/_home_footer.php
+++ Production/home/_home_footer.php
@@ -19,7 +19,8 @@
 					</div>
 				</div>
 			</footer>
-</div>				
+		</div>
+	</div>
 </body>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
