5e2c527b35797d91d2d10b85d22d7542153ec583
ZerryKhoo <zongzhe111@gmail.com>
2026-04-18 13:16:15 +0800

    Delete Production/config/mail.config.php

--- Production/config/mail.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'smtp.gmail.com',
-    'username' => 'leujh-wm25@student.tarc.edu.my',
-    'password' => 'pcbg jprz aidp csqk',
-    'port' => 587,
-    'from_email' => 'leujh-wm25@student.tarc.edu.my',
-    'from_name' => 'CafeDash Support'
-];
